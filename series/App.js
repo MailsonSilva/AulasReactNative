704c20e17ec3284f2ac9462478ce26ac3a724174
MailsonFcSilva <mailsonfcsilva@gmail.com>
2019-12-26 23:50:58 -0300

    Criando 'Router'

--- series/App.js
+++ series/App.js
@@ -1,37 +1,3 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import SeriesApp from './src/SeriesApp';
 
-import LoginScreen from './src/pages/LoginScreen'
-import SeriesPage from './src/pages/SeriesPage'
-
-const AppNavigator = createStackNavigator({
-  'Login': {
-    screen: LoginScreen,
-    navigationOptions: {
-      title: "Bem Vindo!"
-    }
-  },
-  'Main': {
-    screen: SeriesPage,
-    headerTintColor: 'white',
-  }
-},
-{
-    defaultNavigationOptions: {
-      title: "Series",
-      headerTintColor: 'white',
-      headerStyle: {
-        backgroundColor: '#6ca2f7',
-        borderBottomWidth: 1,
-        borderBottomColor: '#C5C5C5'
-      },
-      headerTitleStyle: {
-        color: 'white',
-        fontSize: 30
-      }
-    }
-});
-
-const AppContainer = createAppContainer(AppNavigator);
-
-export default AppContainer;
+export default SeriesApp;
